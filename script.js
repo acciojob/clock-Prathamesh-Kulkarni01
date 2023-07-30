c0ae1514f4c48e5749dc7324c4026044adaf1671
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-30 13:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    let p = document.createElement('p');
+    setInterval(() => {
+		const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+			hourCycle: 'h12',
+        second: "numeric",
+        hour12: true // Use 24-hour format
+    };
+        p.innerHTML = new Date().toLocaleDateString(undefined, options).toUpperCase();
+    }, 1000);
+    document.body.appendChild(p);
+});
